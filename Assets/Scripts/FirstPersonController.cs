f3272e9386db1e0ce7b6d50f19aa571470ed0cf6
Ethan Pinto <ethanpintoa@gmail.com>
2023-02-25 15:00:39 -0500

    Add player movement & rotation

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    [Tooltip("The speed of movement")]
+    public float moveSpeed = 5.0f;
+
+    [Tooltip("The speed of camera rotation")]
+    public float lookSpeed = 5.0f;
+
+    // Camera's current rotation around x-axis and y-axis
+    private float verticalRotation = 0.0f;
+    private float horizontalRotation = 0.0f;
+
+    void Update()
+    {
+        movePlayer();
+        rotateCameraAndPlayer();
+    }
+
+    private void rotateCameraAndPlayer()
+    {
+        verticalRotation -= Input.GetAxis("Mouse Y") * lookSpeed;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90.0f, 90.0f);
+
+        horizontalRotation += Input.GetAxis("Mouse X") * lookSpeed;
+
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0.0f, 0.0f);
+        transform.parent.localRotation = Quaternion.Euler(0.0f, horizontalRotation, 0.0f);
+    }
+
+    private void movePlayer()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.parent.Translate(new Vector3(horizontalMovement, 0.0f, verticalMovement));
+    }
+}
