9441104f260011f7608add7ededb00a90e83c832
Ethan Pinto <ethanpintoa@gmail.com>
2023-03-10 18:25:39 -0500

    Improve player
    Clean up and improve playerController and player object usability

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,31 +1,44 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class FirstPersonController : MonoBehaviour
 {
+    [Header("Movement")]
     [Tooltip("The speed of movement")]
-    public float moveSpeed = 5.0f;
-
+    public float moveSpeed;
     [Tooltip("The speed of camera rotation")]
-    public float lookSpeed = 5.0f;
+    public float lookSpeed;
+    public float verticalLookLimit;
+
+    [Header("Game Objects")]
+    public Camera playerCamera;
+
+    [HideInInspector]
+    public bool canMove = true;
 
     // Camera's current rotation around x-axis and y-axis
-    private float verticalRotation = 0.0f;
-    private float horizontalRotation = 0.0f;
+    private float verticalRotation = 0;
+    private float horizontalRotation = 0;
+
+    private CharacterController characterController;
 
     void Start()
     {
+        characterController = GetComponent<CharacterController>();
+
         // Lock the cursor to the center of the screen
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        MovePlayer();
-        RotateCameraAndPlayer();
+        if (canMove) { RotatePlayer(); }
 
-        // "Toggle cursor lock & visibility"
+        // Toggle cursor lock & visibility
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            canMove = !canMove;
             if (Cursor.lockState == CursorLockMode.Locked)
             {
                 Cursor.lockState = CursorLockMode.None;
@@ -37,21 +50,27 @@ public class FirstPersonController : MonoBehaviour
         }
     }
 
-    private void RotateCameraAndPlayer()
+    void FixedUpdate()
     {
-        verticalRotation -= Input.GetAxis("Mouse Y") * lookSpeed;
-        verticalRotation = Mathf.Clamp(verticalRotation, -90.0f, 90.0f);
-
-        horizontalRotation += Input.GetAxis("Mouse X") * lookSpeed;
-
-        transform.localRotation = Quaternion.Euler(verticalRotation, 0.0f, 0.0f);
-        transform.parent.localRotation = Quaternion.Euler(0.0f, horizontalRotation, 0.0f);
+        if (canMove) { MovePlayer(); }
     }
 
     private void MovePlayer()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        transform.parent.Translate(new Vector3(horizontalMovement, 0.0f, verticalMovement));
+        var horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime;
+        var verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime;
+
+        var motion = transform.TransformDirection(horizontalMovement, 0, verticalMovement);
+        characterController.Move(motion);
+    }
+
+    private void RotatePlayer()
+    {
+        verticalRotation -= Input.GetAxis("Mouse Y") * lookSpeed;
+        verticalRotation = Mathf.Clamp(verticalRotation, -verticalLookLimit, verticalLookLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+        horizontalRotation += Input.GetAxis("Mouse X") * lookSpeed;
+        transform.localRotation = Quaternion.Euler(0, horizontalRotation, 0);
     }
 }
