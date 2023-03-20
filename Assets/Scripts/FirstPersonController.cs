c4118e69c29bae2bfced3c7e6b57c5de69e0ecf6
Ethan Pinto <ethanpintoa@gmail.com>
2023-03-20 19:14:56 -0400

    Re-write player controller
    Use new input system, add gravity, and add jumping

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 
@@ -10,6 +11,13 @@ public class FirstPersonController : MonoBehaviour
     [Tooltip("The speed of camera rotation")]
     public float lookSpeed;
     public float verticalLookLimit;
+    [Tooltip("The downward force to apply to player while not grounded")]
+    public float gravity;
+    [Tooltip("The upward force to apply to player while not grounded")]
+    public float jumpStrenth;
+    [Tooltip("The max vertical force to apply to player while not grounded")]
+    public float maxVelocity;
+
 
     [Header("Game Objects")]
     public Camera playerCamera;
@@ -17,28 +25,29 @@ public class FirstPersonController : MonoBehaviour
     [HideInInspector]
     public bool canMove = true;
 
-    // Camera's current rotation around x-axis and y-axis
-    private float verticalRotation = 0;
-    private float horizontalRotation = 0;
+    private Vector3 localMoveDirection = Vector3.zero;
+    private Vector2 localRotation = Vector2.zero;
 
     private CharacterController characterController;
 
-    void Start()
+    private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+    }
 
+    private void Start()
+    {
         // Lock the cursor to the center of the screen
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void Update()
+    private void Update()
     {
-        if (canMove) { RotatePlayer(); }
-
         // Toggle cursor lock & visibility
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             canMove = !canMove;
+
             if (Cursor.lockState == CursorLockMode.Locked)
             {
                 Cursor.lockState = CursorLockMode.None;
@@ -48,29 +57,59 @@ public class FirstPersonController : MonoBehaviour
                 Cursor.lockState = CursorLockMode.Locked;
             }
         }
+
+        if (!canMove) { return; }
+
+        // Apply rotation
+        playerCamera.transform.localRotation = Quaternion.Euler(localRotation.y, 0, 0);
+        transform.localRotation = Quaternion.Euler(0, localRotation.x, 0);
     }
 
     void FixedUpdate()
     {
-        if (canMove) { MovePlayer(); }
+        // Apply movement
+        var worldMoveDirection = transform.TransformDirection(localMoveDirection);
+        characterController.Move(worldMoveDirection * (moveSpeed * Time.fixedDeltaTime));
+
+        // Apply Gravity
+        // Check if grounded and not jumping
+        if (characterController.isGrounded && localMoveDirection.y < 0)
+        {
+            // -1 and not 0 because physics engine pushs object outside floor.
+            // You need to push object into floor so `isGrounded` is true the
+            // next game update.
+            localMoveDirection.y = -1;
+        }
+        else
+        {
+            localMoveDirection.y -= gravity * Time.fixedDeltaTime;
+            localMoveDirection.y = Mathf.Clamp(localMoveDirection.y, -maxVelocity, maxVelocity);
+        }
     }
 
-    private void MovePlayer()
+    public void Move(InputAction.CallbackContext context)
     {
-        var horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime;
-        var verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime;
-
-        var motion = transform.TransformDirection(horizontalMovement, 0, verticalMovement);
-        characterController.Move(motion);
+        // `input` is already normalized
+        var input = context.ReadValue<Vector2>();
+        localMoveDirection.x = input.x;
+        localMoveDirection.z = input.y;
     }
 
-    private void RotatePlayer()
+    public void Jump(InputAction.CallbackContext context)
     {
-        verticalRotation -= Input.GetAxis("Mouse Y") * lookSpeed;
-        verticalRotation = Mathf.Clamp(verticalRotation, -verticalLookLimit, verticalLookLimit);
-        playerCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+        // if jump button isn't pressed or object isn't grounded
+        if (!context.started || !characterController.isGrounded) { return; }
 
-        horizontalRotation += Input.GetAxis("Mouse X") * lookSpeed;
-        transform.localRotation = Quaternion.Euler(0, horizontalRotation, 0);
+        localMoveDirection.y += jumpStrenth;
+    }
+
+    public void Rotate(InputAction.CallbackContext context)
+    {
+        var input = context.ReadValue<Vector2>();
+
+        localRotation.y -= input.y * lookSpeed;
+        localRotation.y = Mathf.Clamp(localRotation.y, -verticalLookLimit, verticalLookLimit);
+
+        localRotation.x += input.x * lookSpeed;
     }
 }
