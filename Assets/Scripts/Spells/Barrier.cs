f66cdbe86e41653b25ec0d926cdc82771d898e7f
Ethan Pinto <ethanpintoa@gmail.com>
2023-05-07 19:21:23 -0400

    Add barrier destruction

--- Assets/Scripts/Spells/Barrier.cs
+++ Assets/Scripts/Spells/Barrier.cs
@@ -2,11 +2,38 @@ using UnityEngine;
 
 public class Barrier : MonoBehaviour
 {
+    private float hitStopwatch = 0.0f;
+    private readonly float hitAtTime = 3.0f;
+
+    private readonly int breakAfterHitsNum = 2;
+    private int numberOfTimesHit = 0;
+
+    private void Update()
+    {
+        if (numberOfTimesHit > 0)
+        {
+            hitStopwatch += Time.deltaTime;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Spell"))
+        if (!other.CompareTag("Spell")) { return; }
+
+        Destroy(other.gameObject);
+
+        if (numberOfTimesHit == 0)
         {
-            Destroy(other.gameObject);
+            numberOfTimesHit += 1;
+        }
+        else if (hitStopwatch >= hitAtTime)
+        {
+            numberOfTimesHit += 1;
+
+            if (numberOfTimesHit >= breakAfterHitsNum)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
